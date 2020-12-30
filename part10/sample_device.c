63c6a3b57d2fd4b0929cf647d332858d38984456
Denny Dongsung Yang <denny.ds.yang@gmail.com>
2020-12-29 20:42:41 -0800

    part10 sample driver for wait queue example

---
+++ part10/sample_device.c
@@ -0,0 +1,204 @@
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+#include <linux/kthread.h>
+#include <linux/wait.h>
+
+uint32_t read_count = 0;
+static struct task_struct *wait_thread;
+
+DECLARE_WAIT_QUEUE_HEAD(wait_queue_etx);
+
+dev_t dev = 0;
+static struct class *dev_class;
+static struct cdev etx_cdev;
+int wait_queue_flag = 0;
+
+/*
+ * Function prototypes
+ */
+static int	__init etx_driver_init(void);
+static void	__exit etx_driver_exit(void);
+
+/********************** Driver Functions *****************************/
+static int	etx_open(struct inode *inode, struct file *file);
+static int	etx_release(struct inode *inode, struct file *file);
+static ssize_t	etx_read(struct file *filp, char __user *buf, size_t len, loff_t *off);
+static ssize_t 	etx_write(struct file *filp, const char __user *buf, size_t len, loff_t *off);
+
+/*
+ * File operation structure
+ */
+static struct file_operations fops = {
+	.owner 	= THIS_MODULE,
+	.read 	= etx_read,
+	.write 	= etx_write,
+	.release= etx_release,
+	.open	= etx_open,
+};
+
+/*
+ * Thread function
+ */
+static int wait_function(void *unused) {
+	while(1) {
+		printk(KERN_INFO "Waiting Ffor Event...\n");
+		wait_event_interruptible(wait_queue_etx, wait_queue_flag != 0);
+		if(wait_queue_flag == 2) {
+			printk(KERN_INFO "Eventt Came Froom Exit Function \n");
+			return 0;
+		}
+		printk(KERN_INFO "Event Came From Read Function - %d\n", ++read_count);
+		wait_queue_flag = 0;
+	}
+	do_exit(0);
+	return 0;
+}
+
+/*
+ * This function will be caalled when we open teh device file.
+ */
+static int etx_open(struct inode *inode, struct file *file) {
+	printk(KERN_INFO "Devie File Opened...!!!\n");
+	return 0;
+}
+
+static int etx_release(struct inode *inode, struct file *file) {
+	printk(KERN_INFO "Device File Closed...!!!\n");
+	return 0;
+}
+
+/*
+ * This function will be called when we read teh device file
+ */
+static ssize_t etx_read(struct file *filp, char __user *buf, size_t len, loff_t *off) {
+	printk(KERN_INFO "Read Function \n");
+	wait_queue_flag = 1;
+	wake_up_interruptible(&wait_queue_etx);
+	return 0;
+}
+
+/*
+ * This function will be called when we write the device file
+ */
+static ssize_t etx_write(struct file *filp, const char __user *buf, size_t len, loff_t *off) {
+	printk(KERN_INFO "Write Function");
+	return 0;
+}
+
+static int __init etx_driver_init(void) {
+
+	/* Allocating Major number */
+	if ((alloc_chrdev_region(&dev, 0, 1, "etx_dev")) <0) {
+		printk(KERN_INFO "Cannot allocate major number\n");
+		return -1;
+	}
+	printk(KERN_INFO "Major = %d Minor = %d\n", MAJOR(dev), MINOR(dev));
+
+	/* Creating cdev structure */
+	cdev_init(&etx_cdev, &fops);
+	etx_cdev.owner = THIS_MODULE;
+	etx_cdev.ops = &fops;
+
+	/* Adding character device to the system */
+	if ((cdev_add(&etx_cdev, dev, 1)) < 0) {
+		printk(KERN_INFO "Cannot add the device to the system\n");
+		goto r_class;
+	}
+
+	/* Creating struct class */
+	if ((dev_class = class_create(THIS_MODULE, "etx_class")) == NULL) {
+		printk(KERN_INFO "Cannot create the struct class\n");
+		goto r_class;
+	}
+
+	/* Creating device */
+	if ((device_create(dev_class, NULL, dev, NULL, "etx_device")) == NULL) {
+		printk(KERN_INFO "Cannot create the device 1\n");
+		goto r_device;
+	}
+
+	/* Initialize wait queue */
+	init_waitqueue_head(&wait_queue_etx);
+
+	/* Create the kernel thread with name 'mythead' */
+	wait_thread = kthread_create(wait_function, NULL, "WaitThread");
+	if (wait_thread) {
+		printk(KERN_INFO "Thread Created successfully\n");
+		wake_up_process(wait_thread);
+	} else
+		printk(KERN_INFO "Thread Creation failed\n");
+
+	printk(KERN_INFO "Device Driver insert ... Done!!!\n");
+	return 0;
+r_device:
+	class_destroy(dev_class);
+r_class:
+	unregister_chrdev_region(dev, 1);
+	return -1;
+}
+
+
+/*
+ * Module exit function.
+ */
+static void __exit etx_driver_exit(void) {
+	
+	wait_queue_flag = 2;
+	wake_up_interruptible(&wait_queue_etx);
+	device_destroy(dev_class, dev);
+	class_destroy(dev_class);
+	cdev_del(&etx_cdev);
+	unregister_chrdev_region(dev, 1);
+	printk(KERN_INFO "Device Driver Remove... Done!!!\n");
+}
+
+module_init(etx_driver_init);
+module_exit(etx_driver_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Denn Yang <denny.ds.yang@gmail.com>");
+MODULE_DESCRIPTION("Sample Device Driver");
+MODULE_VERSION("1.0");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
